d89199c78ab0e11e8f7f90ce2f1df963222fc23d
sneakerhax <sneakerhax@gmail.com>
2019-07-07 17:50:27 -0700

    fix conflict issue

--- Docker/pyreconer/run_reconer.sh
+++ Docker/pyreconer/run_reconer.sh
@@ -1,8 +1,4 @@
 docker run -it --cidfile cid pyreconer <target>
 cid=`cat cid` 
-<<<<<<< HEAD
 docker cp $cid:/PyReconer/output .
 rm cid
-=======
-docker cp $cid:/reconer/output .
->>>>>>> 0c97a88e5b871db9f903f42f2ee4bca37d3c635a
